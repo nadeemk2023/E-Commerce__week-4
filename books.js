21480285bc6c6f682f925fa37301c8c49ae87c3c
nadeemk2023 <nadeemekhan@gmail.com>
2024-10-31 20:25:43 -0700

    Refactor renderBooks function to use consistent variable naming and add support for displaying book ratings

--- books.js
+++ books.js
@@ -13,7 +13,7 @@ function renderBooks(filter) {
     books.sort((a, b) => b.rating - a.rating);
   }
 
-  const booksHtml = books
+  const booksHTML = books
     .map(book => {
       return `<div class='book'>
     <figure class='book__img--wrapper'>
@@ -21,11 +21,7 @@ function renderBooks(filter) {
     </figure>
     <div class='book__title'>${book.title}</div>
     <div class='book__ratings'>
-      <i class='fas fa-star'></i>
-      <i class='fas fa-star'></i>
-      <i class='fas fa-star'></i>
-      <i class='fas fa-star'></i>
-      <i class='fas fa-star-half-alt'></i>
+      ${ratingsHTML(book.rating)}
     </div>
     <div class='book__price'>
       <span>$${book.originalPrice.toFixed(2)}</span> 
@@ -34,7 +30,19 @@ function renderBooks(filter) {
     })
     .join('');
 
-  booksWrapper.innerHTML = booksHtml;
+  booksWrapper.innerHTML = booksHTML;
+}
+
+function ratingsHTML(rating) {
+  let ratingHTML = '';
+
+  for (let i = 0; i < Math.floor(rating); i++) {
+    ratingHTML += '<i class="fas fa-star"></i>';
+  }
+  if (!Number.isInteger(rating)) {
+    ratingHTML += '<i class="fas fa-star-half-alt"></i>';
+  }
+  return ratingHTML;
 }
 
 function filterBooks() {
