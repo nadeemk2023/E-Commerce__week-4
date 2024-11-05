7435caf2387e76efce113103f838ea47f8becdc9
nadeemk2023 <nadeemekhan@gmail.com>
2024-11-04 22:36:08 -0800

    Refactor sorting logic in renderBooks function to consider salePrice when filtering books by price

--- books.js
+++ books.js
@@ -4,7 +4,10 @@ function renderBooks(filter) {
   const books = getBooks();
 
   if (filter === 'LOW_TO_HIGH') {
-    books.sort((a, b) => a.originalPrice - b.originalPrice);
+    books.sort(
+      (a, b) =>
+        (a.salePrice || a.originalPrice) - (b.salePrice || b.originalPrice)
+    );
   } else if (filter === 'HIGH_TO_LOW') {
     books.sort((a, b) => b.originalPrice - a.originalPrice);
   } else if (filter === 'RATING') {
